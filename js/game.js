8538139178bfab10e8865317f0ff2af8ea410bc1
ZHAOYANNI <zhni@ucm.es>
2020-04-05 18:20:28 +0200

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -6,13 +6,11 @@ export default class Game extends Phaser.Scene {
   }
   preload() {  
   	this.load.image('background', 'assets/images/background.png');
-    //this.load.image('player', 'assets/images/shark-sheet0.png');
     this.load.image('life', 'assets/images/guilife-sheet0.png');
     this.load.image('score', 'assets/images/guiscore-sheet0.png');
     this.load.image('bullet', 'assets/images/guitime-sheet0.png');
     this.load.image('pause', 'assets/images/btnpause-sheet0.png');
     this.load.image('sound', 'assets/images/btnsound-sheet1.png');
-    //this.load.spritesheet('shark', 'assets/images/shark-sheet0.png',);
     //this.load.audio('audio-bounce', ['audio/bounce.ogg', 'audio/bounce.mp3', 'audio/bounce.m4a']);
     this.load.spritesheet('player', 'assets/images/shark-sheet0.png', {frameWidth: 128,frameHeight: 103});
   }
@@ -24,9 +22,8 @@ export default class Game extends Phaser.Scene {
     this.add.image(900, 100, 'bullet');
     this.add.image(1600, 100, 'pause');
     this.add.image(1800, 100, 'sound');
-    
-    this.player = new Player(this, 300, 640);
 
+    //this.player = this.add.sprite(300, 640, 'player');
     this.anims.create({
       key: 'player_normal',
       frames: this.anims.generateFrameNumbers('player', { start: 0, end: 6 }),
@@ -34,20 +31,15 @@ export default class Game extends Phaser.Scene {
       repeat: -1
     });
     
-    /*this.anims.create({
-      key: 'player_attack',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 7 }),
-      frameRate: 10,
-      repeat: -1
-    });
-    this.anims.create({
-      key: 'player_die',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 7 }),
-      frameRate: 10,
-      repeat: -1
-    });*/
+    this.player = player(this,300, 640);
   }
 
   update(time, delta) {    
+    /**
+     * this.input.keyboard.on("keydown_D", () => {
+      this.player.x += 100;
+      this.anims.play('player_normal',true);
+    });
+     */
   }
 }
